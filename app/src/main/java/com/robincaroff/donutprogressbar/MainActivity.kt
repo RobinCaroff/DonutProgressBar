f8ab7ab08d404ea7bf4e180671a795d12b3cd769
Robin Caroff <robin.caroff@gmail.com>
2019-08-30 13:44:46 +0200

    👍 Adds basic animation.

--- app/src/main/java/com/robincaroff/donutprogressbar/MainActivity.kt
+++ app/src/main/java/com/robincaroff/donutprogressbar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.robincaroff.donutprogressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        donutprogressbar_main.maxProgress = 100f
-        donutprogressbar_main.progress = 25f
+        donutprogressbar_main.maxProgress = PROGRESS_TARGET
+
+        updateProgress()
     }
+
+    private fun updateProgress() {
+        Handler().postDelayed( {
+            if(donutprogressbar_main.progress < PROGRESS_TARGET) {
+                donutprogressbar_main.progress++
+                updateProgress()
+            }
+        }, UPDATE_DELAY_MILLISECONDS)
+    }
+
+    companion object {
+        private const val PROGRESS_TARGET = 100f
+        private const val UPDATE_DELAY_MILLISECONDS = 100L
+    }
+
 }
